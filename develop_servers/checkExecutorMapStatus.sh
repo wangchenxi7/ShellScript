da75f2b7838d4d63633474636bc6fe99ec45e372
Chenxi Wang <wangchenxi@g.ucla.edu>
2018-12-19 19:24:15 -0500

    add checkExecutorMapStatus.sh

--- develop_servers/checkExecutorMapStatus.sh
+++ develop_servers/checkExecutorMapStatus.sh
@@ -8,11 +8,19 @@ do
   done
   echo "perf executor: $executorId"
 
+logFile=$1
 
-echo -e "\n\n PID:${executorId}  " >> tmp.log
-echo -e  "\n\n check numa_maps" >> tmp.log
-cat /proc/${executorId}/numa_maps >> tmp.log
-
-echo -e "\n\n check mmap" >> tmp.log
-cat /proc/${executorId}/maps >> tmp.log
-
+if [ -z "$logFile" ]
+then 
+  echo -e "\n\n PID:${executorId}  " >> tmp.log
+  echo -e  "\n\n check numa_maps" >> tmp.log
+  cat /proc/${executorId}/numa_maps >> tmp.log
+  echo -e "\n\n check mmap" >>  tmp.log
+  cat /proc/${executorId}/maps >>  tmp.log
+else
+  echo -e "\n\n PID:${executorId}  " >> ${logFile}.log
+  echo -e  "\n\n check numa_maps" >> ${logFile}.log
+  cat /proc/${executorId}/numa_maps >> ${logFile}.log
+  echo -e "\n\n check mmap" >> ${logFile}.log
+  cat /proc/${executorId}/maps >> ${logFile}.log
+fi
