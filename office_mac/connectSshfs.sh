a527677c1eb8df945322228a14e782e7304e9577
Chenxi Wang <wangchenxi@g.ucla.edu>
2019-03-15 16:50:51 -0700

    office script update

--- office_mac/connectSshfs.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-#connect to server210
-echo "sshfs spark06@10.3.1.210:/home2/spark06 /Users/wcx/Program/spark06_210"
-sshfs spark06@10.3.1.210:/home2/spark06 /Users/wcx/Program/spark06_210
-
-#connect to slave server217
-echo "sshfs spark06@10.3.1.217:/home2/spark06 /Users/wcx/Program/spark06_217"
-sshfs spark06@10.3.1.217:/home2/spark06 /Users/wcx/Program/spark06_217
