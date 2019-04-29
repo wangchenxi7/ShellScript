fa6cf47959413b1c028e97161e930ab5824e69c3
root <root@zion-2.cs.ucla.edu>
2019-04-29 09:47:46 -0700

    server management, add usr and password

--- server_manager/distribute_file_to_cluster.sh
+++ server_manager/distribute_file_to_cluster.sh
@@ -20,14 +20,18 @@ Domainname="cs.ucla.edu"
 	if [ "${Choice}" = "1"  ]
 	then
 
-		Src=""
-		Dest="~"
+		echo "Please input the source file"
+		read	Src
+		echo "Source file is ${Src}"
 
+		echo "Please input destination"
+		read Dest
+		echo "Destination is ${Dest}"
 
 		for Server in paso buckeye glacier   zion-3 zion-4 zion-5 zion-6 zion-7 zion-8 zion-9 zion-10 zion-11
 		do
-			echo "Send files to ${User}@${Server}.${Domainname}:${Dest} "	
-
+			echo "Send files to ${User}@${Server}.${Domainname}:${Dest}/ "	
+			scp -r ${Src}  ${User}@${Server}.${Domainname}:${Dest}/
 
 		done
 
@@ -47,7 +51,7 @@ then
 elif [ ${Choice} = "3" ]
 then
 	## Configurations
-	servers="zion-[3-11]"
+	servers="root@zion-[3-11].cs.ucla.edu,root@paso.cs.ucla.edu,root@buckeye.cs.ucla.edu,root@glacier.cs.ucla.edu"
 
 	echo "Create new user"
 	echo "Please Input user name :"
@@ -56,24 +60,22 @@ then
 	echo "Please input the password:"
 	read password
 
-	echo "Please input the group:"
-	read group
+	#echo "Please input the group:"
+	#read group
 
 	# Create the user 	
-	echo "pdsh -w root@${servers}.cs.ucla.edu useradd -m -d /home/${Newly_created_user} ${Newly_created_user}"
-	pdsh -w root@${servers}.cs.ucla.edu useradd -m -d /home/${Newly_created_user} ${Newly_created_user}
+  echo  " pdsh -w ${servers} useradd -m -d /home/${Newly_created_user} ${Newly_created_user}"
+	pdsh -w ${servers} "useradd -m -d /home/${Newly_created_user} ${Newly_created_user}"
 
 	# Change the password
-	echo "Change password of  ${Newly_created_user}, password : ${password}"
-	echo "pdsh -w root@${servers}.cs.ucla.edu  'printf "%s\n" ${password} ${password}  | passwd ${Newly_created_user}'"
-	pdsh -w root@${servers}.cs.ucla.edu  'printf "%s\n" ${password} ${password}  | passwd ${Newly_created_user}'
+	echo "=> Update password of  ${Newly_created_user} to  ${password}"
+	pdsh -w ${servers}  "printf \"%s\n\" ${password} ${password}  | passwd ${Newly_created_user}"
 
-
-	if [ -n  "${group}"  ]
-	then
-		echo "Add ${Newly_created_user} to Group ${group}"
-		echo "pdsh -w root@${servers}.cs.ucla.edu 'usermod -a -G ${group} ${Newly_created_user}' "
-	fi
+	#if [ -n  "${group}"  ]
+	#then
+#		echo "Add ${Newly_created_user} to Group ${group}"
+#		echo "pdsh -w ${servers} 'usermod -a -G ${group} ${Newly_created_user}' "
+#	fi
 
 
 elif [ "${Choice}" = "4"  ]
