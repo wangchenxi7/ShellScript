9c8c7c0b591eb05c93e118c9e3f854e31fa5dc67
wangchenxi7 <wangchenxi@g.ucla.edu>
2019-04-30 14:14:51 -0700

    update office_mac

--- office_mac/ssh-server.sh
+++ office_mac/ssh-server.sh
@@ -8,8 +8,8 @@ if [ -z "${choice}"  ]
 then
 	echo "Please what to do" 
 	echo " 1 : connect to wcx@Python Server in Lab 496 "
-	echo " 2 : scp files to Python Server in Lab 496 "
-	echo " 3 : connect to ICT@CAS server by VPN "
+	echo " 2 : connect to wcx@paso Server"
+	echo " 3 : connect to wcx@buckeye  server"
 	echo " 4 : connect to plsys@glacier server "
 	echo " 5 : connect to wcx@buckeys server "
 	echo " 6 : connect to wcx@zion-1 server	"
@@ -31,41 +31,43 @@ then
 
 elif [ "${choice}" = "2" ]
 then
-	file_src="${2}"
-	if [ -z "${file_src}" ]
-	then
-		echo "Input the source file"
-		read file_src
-	fi	
+	#file_src="${2}"
+	#if [ -z "${file_src}" ]
+	#then
+#		echo "Input the source file"
+#		read file_src
+#	fi	
 	
-	echo "scp ${file_src} python@131.179.96.132:~"
-	scp ${file_src} python@131.179.96.132:~
-	exit
+	#echo "scp ${file_src} python@131.179.96.132:~"
+	#scp ${file_src} python@131.179.96.132:~
+	#exit
 
+	echo "ssh wcx@paso.cs.ucla.edu"
+	ssh wcx@paso.cs.ucla.edu
+	
 elif	[	"${choice}" = "3"	]
 then	
 
-	echo "ssh -i ~/.ssh_vpn/id_rsa.ict  -oPort=6000 wangchenxi@35.221.158.17"
-	ssh -i ~/.ssh_vpn/id_rsa.ict  -oPort=6000 wangchenxi@35.221.158.17
-	exit
+	#echo "ssh -i ~/.ssh_vpn/id_rsa.ict  -oPort=6000 wangchenxi@35.221.158.17"
+	#ssh -i ~/.ssh_vpn/id_rsa.ict  -oPort=6000 wangchenxi@35.221.158.17
+
+	echo "ssh wcx@buckeye.cs.ucla.edu"
+	ssh wcx@buckeye.cs.ucla.edu	
 
 elif	[	"${choice}" = "4"	]
 then
 	echo "ssh plsys@glacier.cs.ucla.edu"
 	ssh plsys@glacier.cs.ucla.edu
-	exit	
 
 elif	[	"${choice}" = "5"	]
 then
 	echo "ssh wcx@buckeye.cs.ucla.edu"
 	ssh wcx@buckeye.cs.ucla.edu
-	exit	
 
 elif	[	"${choice}" = "6"	]
 then
 	echo "ssh wcx@zion-1.cs.ucla.edu"
 	ssh wcx@zion-1.cs.ucla.edu
-	exit	
 
 
 elif	[	"${choice}" = "7"	]
