fdeb2af5eb6dbcaa19afee79e8ff026167890b6b
wangchenxi7 <wangchenxi@g.ucla.edu>
2019-07-21 15:53:16 -0700

    mount sshfs

--- office_mac/mount-remote-storage.sh
+++ office_mac/mount-remote-storage.sh
@@ -1,10 +1,66 @@
 #! /bin/bash
 
 
-#connect the file system to Lab496 python  server
-#sshfs -o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa   python@131.179.96.132:/home/python    /Users/wcx/Programs/PythonServer
-sshfs   -o sshfs_sync   wcx@131.179.96.132:/mnt/ssd/wcx    /Users/wcx/Programs/PythonServer
+echo "Please select the operations : mount, umount, remount"
 
-#connect to server paso
-sshfs -o sshfs_sync		  wcx@zion-1.cs.ucla.edu:/mnt/ssd/wcx /Users/wcx/Programs/Zion-1Server
+op="$1"
+
+if [ -z "${op}"  ]
+then	
+	read op
+fi
+
+
+## self defined function
+
+mount_remote_disk () {
+
+	echo "Mount remote partitions : PythonServer, Zion-1Server"
+	#connect the file system to Lab496 python  server
+	#sshfs -o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa   python@131.179.96.132:/home/python    /Users/wcx/Programs/PythonServer
+	sshfs   -o sshfs_sync   wcx@131.179.96.132:/mnt/ssd/wcx    /Users/wcx/Programs/PythonServer
+
+	#connect to server paso
+	sshfs -o sshfs_sync		  wcx@zion-1.cs.ucla.edu:/mnt/ssd/wcx /Users/wcx/Programs/Zion-1Server
+
+
+}
+
+
+umount_remote_disk () {
+
+	echo "Un-Mount remote partitions : PythonServer, Zion-1Server"
+
+	umount -f  /Users/wcx/Programs/PythonServer
+
+	umount -f  /Users/wcx/Programs/Zion-1Server
+}
+
+
+
+if [ "${op}" = "mount" ]
+then
+
+	mount_remote_disk	
+
+elif [ "${op}" = "umount"  ]
+then
+
+	umount_remote_disk	
+
+
+elif ["${op}" = "remount"  ]
+then
+
+	umount_remote_disk
+
+	mount_remote_disk	
+
+else
+
+	echo "!!Wrong Choince!!"
+
+fi
+
+
 
