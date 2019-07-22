8033ee28264dc1860ddce9e169ddbaa744bea1d8
wangchenxi7 <wangchenxi@g.ucla.edu>
2019-07-22 16:49:47 -0700

    update sshfs mount

--- office_mac/mount-remote-storage.sh
+++ office_mac/mount-remote-storage.sh
@@ -15,25 +15,29 @@ fi
 
 mount_remote_disk () {
 
-	echo "Mount remote partitions : PythonServer, Zion-1Server"
+	echo "Mount remote partitions : PythonServer, Zion-1Server, Zion-2Server"
 	#connect the file system to Lab496 python  server
 	#sshfs -o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa   python@131.179.96.132:/home/python    /Users/wcx/Programs/PythonServer
 	sshfs   -o sshfs_sync   wcx@131.179.96.132:/mnt/ssd/wcx    /Users/wcx/Programs/PythonServer
 
-	#connect to server paso
+	#connect to server zion-1
 	sshfs -o sshfs_sync		  wcx@zion-1.cs.ucla.edu:/mnt/ssd/wcx /Users/wcx/Programs/Zion-1Server
 
+	#connect to server zion-2
+	sshfs -o sshfs_sync     wcx@zion-2.cs.ucla.edu:/mnt/ssd/wcx /Users/wcx/Programs/Zion-2Server
 
 }
 
 
 umount_remote_disk () {
 
-	echo "Un-Mount remote partitions : PythonServer, Zion-1Server"
+	echo "Un-Mount remote partitions : PythonServer, Zion-1Server, Zion-2Server"
 
 	umount -f  /Users/wcx/Programs/PythonServer
 
 	umount -f  /Users/wcx/Programs/Zion-1Server
+
+	umount -f  /Users/wcx/Programs/Zion-2Server
 }
 
 
