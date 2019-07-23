9b41a13318418f32a6ec0cb2449ce8dbbc9fa48d
root <root@Zion-2.CS.UCLA.EDU>
2019-07-22 18:04:27 -0700

    update nvme target

--- Kernel-dev/nvmeof/nvmeof_target.sh
+++ Kernel-dev/nvmeof/nvmeof_target.sh
@@ -33,12 +33,37 @@ load_client_modules () {
 load_target_modules () {
 
 	echo "Load modules for target/memory server : nvmet,nvmet-rdma, nvme-rdma "
-	sudo modprobe nvmet
-	sudo modprobe nvmet-rdma
-	sudo modprobe nvme-rdama
+	modprobe nvmet
+	modprobe nvmet-rdma
+	modprobe nvme-rdma
 
 	echo "Check the loaded nvme modules"
 	lsmod | grep "nvme"
+
+}
+
+delete_target_modules () {
+
+	echo "Un-load the nvmet modules"
+
+	# delete these modules
+	modprobe -r nvmet
+	modprobe -r nvmet-rdma
+	modprobe -r nvme-rdma
+
+	echo "Check the loaded nvme modules"
+	lsmod | grep "nvme"
+}
+
+reload_target_modules () {
+	
+	echo "Re-Load the modules for Target/Memory server :  nvmet,nvmet-rdma, nvme-rdma"
+
+	# delete the NVMET modules	
+	delete_target_modules	
+
+	# load the NVMET modules
+	load_target_modules
 
 }
 
@@ -50,7 +75,7 @@ load_null_block () {
 	sleep 1
 	#sudo modprobe null_blk nr_devices=1
 	null_block=`ls /dev/ | grep "nullb0"`
-	echo "Current null block : ${null_block}"	
+	echo "Current null block : /dev/${null_block}"	
 
 	if [ "${null_block}" = ""  ]
 	then
@@ -68,13 +93,14 @@ load_null_block () {
 Create_nvme_subsystem () {
 	echo "Create nvme subsystem : ${nvme_nqn}"
 	
-	if [ -z "${nvme_nqn}"  ]
+	if [ -e "${nvme_nqn}"  ]
 	then	
-		mkdir ${nvme_nqn}
-	else
 		echo "File already exist"	
+	else
+		mkdir ${nvme_nqn}
 	fi
 
+	#enable the nvme subsystem
 	cd ${nvme_nqn}	
 	echo 1 > attr_allow_any_host
 }
@@ -87,17 +113,18 @@ Register_block_device () {
 	echo "current path :" 
 	pwd
 
-	if [ -z "namespaces/10"  ]
+	if [ -e "namespaces/10"  ]
 	then
-		mkdir namespaces/10
-	else
 		echo "File : ${nvme_nqn}/namespacs/10 exist."
+	else
+		mkdir namespaces/10
 	fi	
 
 	cd namespaces/10
 	echo "current path :" 
 	pwd
 
+	#Set the store disk & enable it
 	echo -n "${block_device}" > device_path
 	echo 1 > enable
 
@@ -107,7 +134,15 @@ Register_block_device () {
 Start_memory_server () {
 
 	echo "Set network information (${target_ip}:${target_port}) && start the memory server."	
-	mkdir  /sys/kernel/config/nvmet/ports/1
+	
+	if [ -e "/sys/kernel/config/nvmet/ports/1"  ]
+	then
+		echo "/sys/kernel/config/nvmet/ports/1 exist. "
+	else
+		mkdir  /sys/kernel/config/nvmet/ports/1
+	fi	
+
+	# Set the RDMA (over IP) connection information
 	cd /sys/kernel/config/nvmet/ports/1
 	echo "${target_ip}" > addr_traddr
 	echo rdma > addr_trtype
@@ -115,7 +150,12 @@ Start_memory_server () {
 	echo ipv4 > addr_adrfam
 
 	# start the server
-	sudo ln -s ${nvme_nqn} /sys/kernel/config/nvmet/ports/1/subsystems/${nqn}
+	cd /sys/kernel/config/nvmet/ports/1/subsystems
+	echo "current path:"
+	pwd	
+	echo ""
+
+	ln -s ${nvme_nqn} 
 
 }
 
@@ -127,7 +167,8 @@ op="$1"
 
 if [ -z "${op}"  ]
 then
-	echo "Choose operation : load_modules, create_nqn, register_bd, start_mem_server"
+	echo "Choose operation : load_modules, reload_modules (delete-load), create_nqn, register_bd, start_mem_server"
+	echo "Or proceed all the operations : all"
 	read op
 fi
 
@@ -140,7 +181,7 @@ then
 	 Create_nvme_subsystem 
 
 
-elif [ "${op}" = "register_bd"  ]
+elif [ "${op}" = "register_bd"   ]
 then
 	
 	Register_block_device
@@ -160,6 +201,27 @@ then
 
 	echo "Create null block device for test"
 	load_null_block
+
+
+elif [ "${op}" = "reload_modules"  ]
+then
+
+	reload_target_modules
+
+elif [ "${op}" = "all" ]
+then
+	##proceed all the operations 
+
+	load_target_modules 
+
+	load_null_block
+
+	Create_nvme_subsystem 
+
+	Register_block_device
+
+	Start_memory_server
+
 else
 
 	echo "Wrong Operation"
