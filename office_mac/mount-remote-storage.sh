323718334ab6b74e7b5415e29051623c51783bb4
Chenxi Wang <wcx@Chenxi.CS.UCLA.EDU>
2019-10-14 13:04:09 -0700

    update sshfs mounting

--- office_mac/mount-remote-storage.sh
+++ office_mac/mount-remote-storage.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 
-echo "Please select the operations : mount, umount, remount"
 
 
 ## Global variables
@@ -18,24 +17,55 @@ op="$1"
 
 if [ -z "${op}"  ]
 then	
+	echo "Please select the operations : mount, umount, remount"
 	read op
 fi
+
+
+## The target servers to be mounted
+# Defualt : Zion-1
+# all Python, Zion-1, Zion-2
+# Can choose Python, Zion-1, Zion-2 seperately
+
+target_server="$2"
+if [ -z "${target_server}"  ]
+then
+	echo " Slect the mounted server :"
+	echo " zion-1 OR NULL(enter)"
+	echo " all"
+	
+	read target_server
+fi
+
 
 
 ## self defined function
 
 mount_remote_disk () {
 
-	echo "Mount remote partitions : PythonServer, Zion-1Server, Zion-2Server"
-	#connect the file system to Lab496 python  server
-	#sshfs -o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa   python@131.179.96.132:/home/python    ${home_dir}/Programs/PythonServer
-	sshfs   -o sshfs_sync   wcx@131.179.96.132:/mnt/ssd/wcx    ${home_dir}/Programs/PythonServer
+	if [ -z "${target_server}"  ]
+	then
+		echo "Default : mount Zion-1"	
+		#connect to server zion-1
+		sshfs -o sshfs_sync		  wcx@zion-1.cs.ucla.edu:/mnt/ssd/wcx ${home_dir}/Programs/Zion-1Server
 
-	#connect to server zion-1
-	sshfs -o sshfs_sync		  wcx@zion-1.cs.ucla.edu:/mnt/ssd/wcx ${home_dir}/Programs/Zion-1Server
+	elif [ "${target_server}" = "all"  ]
+	then
+	
+		echo "all : Mount Python, Zion-1, Zion-2"
+		
+		#Connect to server Python
+		sshfs   -o sshfs_sync   wcx@python.cs.ucla.edu:/mnt/ssd/wcx    ${home_dir}/Programs/PythonServer
 
-	#connect to server zion-2
-	sshfs -o sshfs_sync     wcx@zion-2.cs.ucla.edu:/mnt/ssd/wcx ${home_dir}/Programs/Zion-2Server
+		#connect to server zion-1
+		sshfs -o sshfs_sync		  wcx@zion-1.cs.ucla.edu:/mnt/ssd/wcx ${home_dir}/Programs/Zion-1Server
+
+		#connect to server zion-2
+		sshfs -o sshfs_sync     wcx@zion-2.cs.ucla.edu:/mnt/ssd/wcx ${home_dir}/Programs/Zion-2Server
+
+	else
+		echo "Wrong Target Server \n"
+	fi
 
 }
 
@@ -44,11 +74,24 @@ umount_remote_disk () {
 
 	echo "Un-Mount remote partitions : PythonServer, Zion-1Server, Zion-2Server"
 
-	umount -f  ${home_dir}/Programs/PythonServer
+	if [ -z "${target_server}"  ]
+	then
+		echo "Default : Unmount Zion-1"	
+		umount -f  ${home_dir}/Programs/PythonServer
 
-	umount -f  ${home_dir}/Programs/Zion-1Server
+	elif [ "${target_server}" = "all"  ]
+	then
+	
+		echo "all : Unmount Python, Zion-1, Zion-2"
+		umount -f  ${home_dir}/Programs/PythonServer
 
-	umount -f  ${home_dir}/Programs/Zion-2Server
+		umount -f  ${home_dir}/Programs/Zion-1Server
+
+		umount -f  ${home_dir}/Programs/Zion-2Server
+		
+	else
+		echo "Wrong Target Server \n"
+	fi
 }
 
 
