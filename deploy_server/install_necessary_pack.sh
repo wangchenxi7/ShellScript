a6996de083c55889a5e3ec972b13c6ce45884f18
Chenxi Wang <wangchenxi@g.ucla.edu>
2020-05-22 02:43:47 -0700

    add more lib to deploy sh

--- deploy_server/install_necessary_pack.sh
+++ deploy_server/install_necessary_pack.sh
@@ -39,3 +39,10 @@ yum install -y rpm-build sh-utils tar xmlto xz zlib-devel
 ## CGroup
 yum install -y libcgroup libcgroup-tools
 
+
+## Install RDMA
+yum install -y libibverbs.x86_64  rdma-core-devel rdma-core.x86_64 librdmacm.x86_64
+
+##
+# For build OpenJDK
+yum install -y  cups-devel cups-devel fontconfig-devel alsa-lib-devel  libXtst-devel libXt-devel libXrender-devel libXrandr-devel libXi-devel
