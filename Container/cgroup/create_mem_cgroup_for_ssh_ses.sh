ebe545df062890f3d320ee1f68be93bb28b46140
Chenxi Wang <wangchenxi@g.ucla.edu>
2020-08-05 22:06:42 -0700

    modify cgroup sh

--- Container/cgroup/create_mem_cgroup_for_ssh_ses.sh
+++ Container/cgroup/create_mem_cgroup_for_ssh_ses.sh
@@ -4,28 +4,48 @@
 # Create a memory cgroup for current ssh session and the process launched by it.
 # The cgroup will be deleted after exit the ssh session or reboot ?
 
-mem_size=$1
 
-if [ -z "${mem_size}"  ]
+if [ -z ${operation} ]
 then
-	echo "Used default memroy size limitations, 4g"
-	mem_size="11g"
-else
-	echo "Memory size limitations: ${mem_size}"
+	echo "Choose operation: create, delete "
+	read operation
 fi
 
 
-user=`whoami`
+if [ "${operation}" = "create"  ]
+then
 
-#1 Create 
-echo "sudo cgcreate -t ${user} -a ${user} -g memory:/memctl"
-sudo cgcreate -t ${user} -a ${user} -g memory:/memctl
+	mem_size=$1
+
+	if [ -z "${mem_size}"  ]
+	then
+		echo "Used default memroy size limitations, 10g"
+		mem_size="10g"
+	else
+		echo "Memory size limitations: ${mem_size}"
+	fi
 
 
-#2 Limi the memory size 
-echo "${mem_size} > /sys/fs/cgroup/memory/memctl/memory.limit_in_bytes"
-sudo echo ${mem_size} > /sys/fs/cgroup/memory/memctl/memory.limit_in_bytes
+	user=`whoami`
 
+	#1 Create 
+	echo "sudo cgcreate -t ${user} -a ${user} -g memory:/memctl"
+	sudo cgcreate -t ${user} -a ${user} -g memory:/memctl
+
+
+	#2 Limi the memory size 
+	echo "${mem_size} > /sys/fs/cgroup/memory/memctl/memory.limit_in_bytes"
+	sudo echo ${mem_size} > /sys/fs/cgroup/memory/memctl/memory.limit_in_bytes
+
+elif [	${operation} = "delete"	]
+then 
+	echo " sudo cgdelete memory:/memctl"
+	sudo cgdelete memory:/memctl
+
+else
+	echo "!! Wrong operation !!"
+	exit
+fi
 
 #3 Add current ssh session into it
 # $$ is the pid of the shellscript, not the sheel.
