e0b8d9853682712ac744d70b601d9d156ffba2ee
Chenxi Wang <wangchenxi@g.ucla.edu>
2021-08-27 19:40:24 -0700

    update diff sh

--- tools/diff_to_patch.sh
+++ tools/diff_to_patch.sh
@@ -2,16 +2,33 @@
 
 ## Configurations
 
-old_repo=$1
-new_repo=$2
+old_repo="Semeru"
+new_repo="Semeru-new"
+
+if [ -z "${HOME}" ]
+then
+  echo "Please set HOME"
+  exit 
+fi
+home_dir=${HOME}
+echo "home dir is ${home_dir}"
+
 
 if [ -z "${old_repo}" ]
-  echo "Input old_repo"
+  echo "Input old_repo name. start from ${home_dir}:"
   read old_repo
 fi
 echo "The old repo: ${old_repo}"
 
 
+if [ -z "${new_repo}" ]
+  echo "Input new_repo name. start from ${home_dir}:"
+  read new_repo
+fi
+echo "The new repo: ${new_repo}"
 
 
-diff --new-file  --exclude=".git" --exclude=".vscode-ctags"   -ru OpenSourceCode/Semeru   ./Semeru > semeru_nov_3.patch
+
+
+#diff --new-file  --exclude=".git" --exclude=".vscode-ctags"   -ru OpenSourceCode/Semeru   ./Semeru > semeru_nov_3.patch
+diff --new-file  --exclude=".git" --exclude=".vscode-ctags"   -ru ${home_dir}/${old_repo}   ${home_dir}/${new_repo} > semeru_aug_27.patch
