3ca5e0bfb90d68c59d1453fb2bf9f1ba3343565f
Chenxi Wang <wangchenxi7@gmail.com>
2022-06-06 11:05:48 +0000

    add sh for alphafold

---
+++ OpenFold/search_database.py
@@ -0,0 +1,167 @@
+# Download the necessary libraries
+from IPython.utils import io
+import os
+import subprocess
+import tqdm.notebook
+
+
+
+
+#@title Enter the amino acid sequence to fold ⬇️
+sequence = 'MAAHKGAEHHHKAAEHHEQAAKHHHAAAEHHEKGEHEQAAHHADTAYAHHKHAEEHAAQAAKHDAEHHAPKPH'  #@param {type:"string"}
+
+MIN_SEQUENCE_LENGTH = 16
+MAX_SEQUENCE_LENGTH = 2500
+
+# Remove all whitespaces, tabs and end lines; upper-case
+sequence = sequence.translate(str.maketrans('', '', ' \n\t')).upper()
+aatypes = set('ACDEFGHIKLMNPQRSTVWY')  # 20 standard aatypes
+if not set(sequence).issubset(aatypes):
+  raise Exception(f'Input sequence contains non-amino acid letters: {set(sequence) - aatypes}. AlphaFold only supports 20 standard amino acids as inputs.')
+if len(sequence) < MIN_SEQUENCE_LENGTH:
+  raise Exception(f'Input sequence is too short: {len(sequence)} amino acids, while the minimum is {MIN_SEQUENCE_LENGTH}')
+if len(sequence) > MAX_SEQUENCE_LENGTH:
+  raise Exception(f'Input sequence is too long: {len(sequence)} amino acids, while the maximum is {MAX_SEQUENCE_LENGTH}. Please use the full AlphaFold system for long sequences.')
+
+print("Input sequence:",sequence, " is valid.")
+
+
+# --- Python imports ---
+import sys
+sys.path.append('/opt/conda/lib/python3.7/site-packages')
+import os
+os.environ['TF_FORCE_UNIFIED_MEMORY'] = '1'
+os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '2.0'
+
+from urllib import request
+from concurrent import futures
+from google.colab import files
+import json
+from matplotlib import gridspec
+import matplotlib.pyplot as plt
+import numpy as np
+import py3Dmol
+import torch
+
+from openfold import config
+from openfold.data import feature_pipeline
+from openfold.data import parsers
+from openfold.data import data_pipeline
+from openfold.data.tools import jackhmmer
+from openfold.model import model
+from openfold.np import protein
+from openfold.np.relax import relax
+from openfold.np.relax import utils
+from openfold.utils.import_weights import import_jax_weights_
+from openfold.utils.tensor_utils import tensor_tree_map
+
+from IPython import display
+from ipywidgets import GridspecLayout
+from ipywidgets import Output
+
+
+# For asin, always use the database stored in storage.googleapis.com/alphafold-colab-asia
+source='-asia'
+
+
+
+# --- Search against genetic databases ---
+with open('target.fasta', 'wt') as f:
+  f.write(f'>query\n{sequence}')
+
+
+# Run the search against chunks of genetic databases (since the genetic
+# databases don't fit in Colab ramdisk).
+
+jackhmmer_binary_path = '/usr/bin/jackhmmer'
+dbs = []
+
+num_jackhmmer_chunks = {'uniref90': 59, 'smallbfd': 17, 'mgnify': 71}
+total_jackhmmer_chunks = sum(num_jackhmmer_chunks.values())
+#with tqdm.notebook.tqdm(total=total_jackhmmer_chunks, bar_format=TQDM_BAR_FORMAT) as pbar:
+# Use the default bar format 
+with tqdm.notebook.tqdm(total=total_jackhmmer_chunks) as pbar:
+  def jackhmmer_chunk_callback(i):
+    pbar.update(n=1)
+
+  pbar.set_description('Searching uniref90')
+  jackhmmer_uniref90_runner = jackhmmer.Jackhmmer(
+      binary_path=jackhmmer_binary_path,
+      n_cpu=20,
+      database_path=f'/root/database/uniref90.fasta',
+      get_tblout=True,
+      num_streamed_chunks=num_jackhmmer_chunks['uniref90'],
+      streaming_callback=jackhmmer_chunk_callback,
+      z_value=135301051)
+  dbs.append(('uniref90', jackhmmer_uniref90_runner.query('target.fasta')))
+
+  pbar.set_description('Searching smallbfd')
+  jackhmmer_smallbfd_runner = jackhmmer.Jackhmmer(
+      binary_path=jackhmmer_binary_path,
+      #database_path=f'https://storage.googleapis.com/alphafold-colab{source}/latest/bfd-first_non_consensus_sequences.fasta',
+      database_path=f'/root/database/bfd-first_non_consensus_sequences.fasta',
+      get_tblout=True,
+      num_streamed_chunks=num_jackhmmer_chunks['smallbfd'],
+      streaming_callback=jackhmmer_chunk_callback,
+      z_value=65984053)
+  dbs.append(('smallbfd', jackhmmer_smallbfd_runner.query('target.fasta')))
+
+  pbar.set_description('Searching mgnify')
+  jackhmmer_mgnify_runner = jackhmmer.Jackhmmer(
+      binary_path=jackhmmer_binary_path,
+      #database_path=f'https://storage.googleapis.com/alphafold-colab{source}/latest/mgy_clusters_2019_05.fasta',
+      database_path=f'/root/databse/mgy_clusters_2018_12.fa',
+      get_tblout=True,
+      num_streamed_chunks=num_jackhmmer_chunks['mgnify'],
+      streaming_callback=jackhmmer_chunk_callback,
+      z_value=304820129)
+  dbs.append(('mgnify', jackhmmer_mgnify_runner.query('target.fasta')))
+
+
+# --- Extract the MSAs and visualize ---
+# Extract the MSAs from the Stockholm files.
+# NB: deduplication happens later in data_pipeline.make_msa_features.
+
+mgnify_max_hits = 501
+
+msas = []
+deletion_matrices = []
+full_msa = []
+for db_name, db_results in dbs:
+  unsorted_results = []
+  for i, result in enumerate(db_results):
+    msa, deletion_matrix, target_names = parsers.parse_stockholm(result['sto'])
+    e_values_dict = parsers.parse_e_values_from_tblout(result['tbl'])
+    e_values = [e_values_dict[t.split('/')[0]] for t in target_names]
+    zipped_results = zip(msa, deletion_matrix, target_names, e_values)
+    if i != 0:
+      # Only take query from the first chunk
+      zipped_results = [x for x in zipped_results if x[2] != 'query']
+    unsorted_results.extend(zipped_results)
+  sorted_by_evalue = sorted(unsorted_results, key=lambda x: x[3])
+  db_msas, db_deletion_matrices, _, _ = zip(*sorted_by_evalue)
+  if db_msas:
+    if db_name == 'mgnify':
+      db_msas = db_msas[:mgnify_max_hits]
+      db_deletion_matrices = db_deletion_matrices[:mgnify_max_hits]
+    full_msa.extend(db_msas)
+    msas.append(db_msas)
+    deletion_matrices.append(db_deletion_matrices)
+    msa_size = len(set(db_msas))
+    print(f'{msa_size} Sequences Found in {db_name}')
+
+deduped_full_msa = list(dict.fromkeys(full_msa))
+total_msa_size = len(deduped_full_msa)
+print(f'\n{total_msa_size} Sequences Found in Total\n')
+
+aa_map = {restype: i for i, restype in enumerate('ABCDEFGHIJKLMNOPQRSTUVWXYZ-')}
+msa_arr = np.array([[aa_map[aa] for aa in seq] for seq in deduped_full_msa])
+num_alignments, num_res = msa_arr.shape
+
+fig = plt.figure(figsize=(12, 3))
+plt.title('Per-Residue Count of Non-Gap Amino Acids in the MSA')
+plt.plot(np.sum(msa_arr != aa_map['-'], axis=0), color='black')
+plt.ylabel('Non-Gap Count')
+plt.yticks(range(0, num_alignments + 1, max(1, int(num_alignments / 3))))
+plt.show()
+
